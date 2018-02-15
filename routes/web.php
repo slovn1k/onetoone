8786a659e4326e80c5a9688711f4813d9a1caf22
Turcanu Alexandr <a.turcanu@software-dep.net>
2018-02-15 13:28:15 +0200

    CRUD one to one relationship
    CRUD example-create, read, update, delete using one to one relationship in laravel.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
+use App\User;
+use App\Adress;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert', function() {
+    $user = User::findOrFail(1);
+
+    $address = new Adress(['name' => 'Muzicescu 21 B']);
+
+    $user->adress()->save($address);
+});
+
+Route::get('/update', function (){
+   $adress = Adress::where('user_id', '1')->first();
+   $adress->name = 'New updated name';
+   $adress->save();
+});
+
+Route::get('/read', function (){
+   $user = User::findOrFail(1);
+   return $user->adress->name;
+});
+
+
+Route::get('/delete/{id}', function ($id) {
+    $user = User::findOrFail($id);
+    $user->delete();
 });
